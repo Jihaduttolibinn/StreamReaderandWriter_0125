7a7655048082cd360e3269566afca6365257a16d
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-06-03 10:29:19 +0700

    Awal program

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -7,5 +7,11 @@ using namespace std;
 
 int main()
 {
-
+	cout << "Awal program" << endl; // penanda 1:....
+	try {
+		array<int, 3> data = { 1,2,3 };
+		//pesan array integer 3 element
+		cout << data.at(5) << endl;
+		//memamnggil array elemen ke 5
+	}
 }
