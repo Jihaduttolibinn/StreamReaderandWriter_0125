dde25958e614f28e4c3fe65e9a423b660be73194
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-06-03 10:08:07 +0700

    Menampilkan file

--- StreamReaderandWriter_0125/StreamReaderandWriter_0125.cpp
+++ StreamReaderandWriter_0125/StreamReaderandWriter_0125.cpp
@@ -46,8 +46,12 @@ int main()
 			//dan tampilkan di sini
 			cout << baris << '\n';
 		}
+		//tutup file tersebut setelah selesai
+		infile.close();
 	}
-
+	//Jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 
 
 }
