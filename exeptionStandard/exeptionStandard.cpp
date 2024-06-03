8d2475cb6775c1b6bff81ed24a4b35747db67d66
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-06-03 10:26:31 +0700

    Memasukkan int main

---
+++ exeptionStandard/exeptionStandard.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan di gunakan 
+#include <array>
+//untuk obyek array yang akan di gunakan 
+using namespace std;
+
+int main()
+{
+
+}
